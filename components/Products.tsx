3c0c5734a7bd3425b993716d6c8e411d6efae6ac
Astropal <bastien.dupont@viacesi.fr>
2022-06-17 10:49:24 +0200

    - Main products grid && products style -

---
+++ components/Products.tsx
@@ -0,0 +1,68 @@
+import type { NextPage } from 'next'
+import { useEffect, useState } from "react";
+import styles from '../styles/Home.module.css'
+import Layout from "@components/Layout";
+import Food from "@svgs/Food.svg";
+import products from "../src/products.json";
+
+interface IProducts {
+    id: number;
+    name?: string;
+    img?: string;
+    rate?: string;
+    fav?: boolean;
+    offer?: boolean;
+  }
+
+const Products: NextPage = () => {
+    console.log(products)
+
+    const dataLoader = (): Promise<IProducts[]> => {
+        const hmm: IProducts[] = products as IProducts[];
+        return new Promise((res) => res(hmm));
+    };
+
+    const [data, setData] = useState<IProducts[]>([]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            dataLoader().then((res) => {
+            setData(res);
+          });
+        }, Math.random() * 1000);
+        return () => clearTimeout(timer);
+      }, []);
+
+  return (
+    <div className="product-grid">
+        <div className="main-left-side">
+            <div className="products-menu">
+                <div className="products-menu-temp"></div>
+            </div>
+        </div>
+        <div className="main-right-side">
+            <div className="products-section">
+                <div className="products-box">
+                    
+                    {data.map((datum) => (
+                        <li className="products-li">
+                            <div className="products-pos">
+                                <a className="products-a" href={"/" + datum.id}>
+                                    <img className="products-img" src={datum.img}></img>
+                                    <div className="products-info">
+                                        <h3 className="product-title">{datum.name}</h3>
+                                        <span className="product-rate">{datum.rate}</span>
+                                    </div>
+                                </a>
+                            </div>
+                        </li>
+                    ))}
+                            
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Products
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import '../styles/Home.module.css'
 import Layout from "@components/Layout";
 import Category from "@components/Category";
+import Products from '@components/Products';
 import Food from "@svgs/Food.svg";
 
 const Home: NextPage = () => {
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
           <Food />
           <br /><span className="font-semibold">Veuillez réessayer après vous être connecté.</span>
          </div>
+      <Products></Products>
     </Layout>
   )
 }
