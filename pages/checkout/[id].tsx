e44bcdddff74ec894015fa64f83adde115a9f42c
Astropal <bastien.dupont@viacesi.fr>
2022-06-30 17:09:58 +0200

    - Cart checkout -

---
+++ pages/checkout/[id].tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { getUserState } from '@src/redux/token.Slicers';
+
+
+const checkout: NextPage = () => {
+
+  return (
+    <>
+    <i className="loader --7"></i>
+    </>
+  )
+}
+
+export default checkout
